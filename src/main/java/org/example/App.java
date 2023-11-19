e5f0ebeb8982503c25bf9425c8c7e19800528fb2
sik2 <bickwang123@gmail.com>
2023-11-19 17:43:11 +0900

    5nd-1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -7,10 +9,15 @@ public class App {
     App (Scanner sc) {
         this.sc = sc;
     }
+
+    List<WiseSaying> wiseSayingList = new ArrayList<>();
+
     void run () {
+
         long lastId = 1;
         System.out.println("== 명언 앱 ==");
         while (true) {
+
             System.out.print("명령) ");
             String command = this.sc.nextLine().trim();
             
@@ -22,7 +29,15 @@ public class App {
                 System.out.print("작가 : ");
                 String author = this.sc.nextLine().trim();
                 System.out.printf("%d번 명언이 등록되었습니다.\n", lastId);
+
+                WiseSaying wiseSaying = new WiseSaying(lastId, author, content);
+                wiseSayingList.add(wiseSaying);
+
                 lastId++;
+            } else if (command.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                System.out.println(wiseSayingList.size());
             }
         }
     }
---
+++ src/main/java/org/example/WiseSaying.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class WiseSaying {
+    private long id;
+    private String content;
+    private String author;
+
+    WiseSaying (long id, String author, String content) {
+        this.id = id;
+        this.author = author;
+        this.content = content;
+    }
+
+}
